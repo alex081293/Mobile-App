80e63be2e4cf6eba02e51d2d7068c7fe43e216aa
alex081293 <alex.11.moran@gmail.com>
2014-09-12 00:39:07 -0400

    Database attempts
    Set up the web server and database.  Set up a site where we're able to
    make queries through $_GET and returned json encoded. Attempting to make
    a request and get that data.

--- src/com/usfMobileApp/mobile_app/MainActivity.java
+++ src/com/usfMobileApp/mobile_app/MainActivity.java
@@ -1,9 +1,25 @@
 package com.usfMobileApp.mobile_app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.*;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,8 +27,61 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final TextView display = (TextView)findViewById(R.id.display);
+		
+		Button connectButton = (Button)findViewById(R.id.connectButton);
+		Button resetButton = (Button)findViewById(R.id.resetButton);
+		
+		connectButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try {
+					dbMakeQuery("SELECT * FROM users");
+				} catch (Exception e) {
+					display.setText("It Failed");
+				}
+			}
+		});
+		resetButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				display.setText("Hello World");
+			}
+		});			
 	}
+	
+	// Sends a request to our web server with the requery in the param, 
+	// hopefully will one day return what we want
+	public void dbMakeQuery(String query) throws Exception {
+		final TextView display = (TextView)findViewById(R.id.display);
+		String url = "http://usfandroidapp.net63.net/mysqlConnect.php?q=" + query;		
+		
+		HttpClient httpclient = new DefaultHttpClient();
+	    HttpGet httpget = new HttpGet(url); 
 
+	    HttpResponse response = null;
+		try {
+			response = httpclient.execute(httpget);
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+
+		BufferedReader rd;
+		try {
+			rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			try {
+				while ((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	    
+		    display.setText(result.toString());
+		} catch (IllegalStateException | IOException e1) {
+			e1.printStackTrace();
+		}	    
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
